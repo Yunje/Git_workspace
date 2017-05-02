a9b5e9936ef7c521f19ec6ac80d6bd76ddd2298b
Yunje <jyjgood888@gmail.com>
2017-05-02 21:21:37 +0900

    Add: Init CSharpStudy Project in Study Folder

---
+++ Study/CSharpStudy/CSharpStudy/Helloworld.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharpStudy
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//        }
+//    }
+//}
+
+
+using System;
+
+namespace CSharpStudy
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
